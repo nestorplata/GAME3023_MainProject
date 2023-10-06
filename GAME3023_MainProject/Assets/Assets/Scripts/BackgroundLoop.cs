d46f3f902f7f41ea404f792fcd62a63d0352306c
nestorplata <62219726+nestorplata@users.noreply.github.com>
2023-10-06 18:58:05 -0400

    Added BackGround Cave as well as collisions

---
+++ GAME3023_MainProject/Assets/Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Profiling;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundLoop : MonoBehaviour
+{
+
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float _x, _y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _img = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
+    }
+}
