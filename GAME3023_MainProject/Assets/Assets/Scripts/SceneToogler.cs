1a5ffe27a494229f10d0ab1778e79412347bc4c2
nestorplata <62219726+nestorplata@users.noreply.github.com>
2023-10-06 20:57:18 -0400

    StartScren with animations

--- GAME3023_MainProject/Assets/Assets/Scripts/SceneToogler.cs
+++ GAME3023_MainProject/Assets/Assets/Scripts/SceneToogler.cs
@@ -2,28 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEditor;
 
 public class SceneToogler : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void MoveToScene(int sceneID)
     {
             SceneManager.LoadScene(sceneID);
-
+        transform.localScale = Vector3.one/2;
     }
     public void Quit()
     {
         Application.Quit();
+        EditorApplication.ExitPlaymode();
+        transform.localScale = Vector3.one / 2;
 
     }
+
+
 }
